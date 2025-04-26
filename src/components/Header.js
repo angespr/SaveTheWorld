bbb737f84f9b995b39d9b982ce46f52f24467857
Ayleen <ayleenpiteotarpy@gmail.com>
2025-04-25 19:51:50 -0700

    update header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,45 @@
 import '../styles/Header.css';
 import Logo from '../assets/logo.png';
 import Profile from '../assets/profile.png';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+
+  const handleProfileClick = () => { navigate('/profile'); };
+  const handleCreateRequestClick = () => { navigate('/create-request'); };
+  const handleLogoClick = () => { navigate('/'); };
+
   return (
     <div className="header">
       <div className="header-left">
-        <img src={Logo} alt="Logo" className="logo header-img" />
-        <h1 className="header-title">juvo</h1>
+        <img
+          src={Logo}
+          alt="Logo"
+          className="logo header-img"
+          onClick={handleLogoClick}
+        />
+        <h1
+          className="header-title"
+          onClick={handleLogoClick}
+        >
+          juvo
+        </h1>
       </div>
 
       <div className="header-right">
         <h1 className="my-requests">My Requests</h1>
-        <i class="header-icon fa-solid fa-circle-plus"></i>
+        <i
+          class="header-icon fa-solid fa-circle-plus"
+          onClick={handleCreateRequestClick}>
+        </i>
         <i class="header-icon fa-solid fa-message"></i>
-        <img src={Profile} alt="Profile" className="profile-icon header-img" />
+        <img
+          src={Profile}
+          alt="Profile"
+          className="profile-icon header-img"
+          onClick={handleProfileClick}
+        />
       </div>
     </div>
   );
