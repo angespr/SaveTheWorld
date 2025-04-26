8695afafb9ae52684a141c22234f5b0d98452af0
Ayleen <ayleenpiteotarpy@gmail.com>
2025-04-26 11:52:01 -0700

    add recommended

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,6 +1,7 @@
 import '../../styles/homepage/Homepage.css';
 import Header from '../Header';
 import SearchBar from './SearchBar';
+import Recommended from './Recommended';
 import React from 'react';
 
 function Homepage() {
@@ -8,6 +9,7 @@ function Homepage() {
     <div className="homepage">
       <Header />
       <SearchBar />
+      <Recommended />
     </div>
   );
 }
