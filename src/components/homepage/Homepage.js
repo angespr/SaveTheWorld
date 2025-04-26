b39df70ada3a0a25d3cd02e765cd48497ef09bed
Ayleen <ayleenpiteotarpy@gmail.com>
2025-04-25 18:42:48 -0700

    move header into its own component

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,19 +1,12 @@
 import '../../styles/homepage/Homepage.css';
-import Logo from '../../assets/logo.png';
-import Profile from '../../assets/profile.png';
+import Header from '../Header';
 import SearchBar from './SearchBar';
 import React from 'react';
 
 function Homepage() {
   return (
     <div className="homepage">
-      <div className="header">
-        <div className="header-left">
-          <img src={Logo} alt="Logo" className="logo header-img" />
-          <h1 className="title">juvo</h1>
-        </div>
-        <img src={Profile} alt="Profile" className="profile header-img" />
-      </div>
+      <Header />
       <SearchBar />
     </div>
   );
