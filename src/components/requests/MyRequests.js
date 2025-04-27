bc4bf417ca00af0140f5361edd8ecdea70e141bc
Ayleen <ayleenpiteotarpy@gmail.com>
2025-04-26 18:53:28 -0700

    oops didn't realize i was on main

--- src/components/requests/MyRequests.js
+++ src/components/requests/MyRequests.js
@@ -6,7 +6,6 @@ function MyRequests() {
   const userId = localStorage.getItem('token');
   return (
     <div className="my-requests-wrapper">
-      <h2>token: {userId}</h2>
       <Header />
       <div className="my-requests-content">
         <Requests
