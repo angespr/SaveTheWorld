f26d69eaa15a67201156d814ddd93cd0bf59c02f
angespr <ange41@uw.edu>
2025-04-26 20:52:16 -0700

    homepage edit and test images

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -12,7 +12,7 @@ function Homepage() {
       <Header />
       <SearchBar />
       <Recommended />
-      <Requests header="All Requests"/>
+      <Requests header="All Requests Near You"/>
     </div>
   );
 }
